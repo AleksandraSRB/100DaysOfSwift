479c2367af892cea577714b25c1234f7f1fafdd3
Aleksandra <94380380+AleksandraSRB@users.noreply.github.com>
2024-04-22 14:57:45 +0200

    Add files via upload

---
+++ Project 10 Names to faces - CollectionView/Challange 3 - CollectionView /Project 1/PictureCell.swift
@@ -0,0 +1,16 @@
+//
+//  PictureCell.swift
+//  Project 1
+//
+//  Created by Aleksandra Sobot on 17.11.23..
+//
+
+import UIKit
+
+class PictureCell: UICollectionViewCell {
+    
+    @IBOutlet var image: UIImageView!
+    @IBOutlet var title: UILabel!
+    
+    
+}
